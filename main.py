180ce8880c61531c30fd5e5982238164ff112888
deathehra <jarvidniies@gmail.com>
2026-01-20 22:45:59 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-from bot import Bot
-import pyrogram.utils
+import asyncio
+asyncio.set_event_loop(asyncio.new_event_loop())
 
-pyrogram.utils.MIN_CHANNEL_ID = -1002381894045
+import pyrogram.utils
+pyrogram.utils.MIN_CHANNEL_ID = -1002275431283
+
+from bot import Bot
 
 if __name__ == "__main__":
     Bot().run()
